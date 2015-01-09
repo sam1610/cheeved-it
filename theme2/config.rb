efd4ac90ab407be64cdbb41b06ce2e7104f817aa
Grant Schulte <schulte.grant@gmail.com>
2015-01-08 21:43:57 -0500

    Initializes repo

---
+++ theme2/config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+css_dir = "/styles"
+sass_dir = "styles/scss"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :compressed
+environment = :development
+
+relative_assets = true
